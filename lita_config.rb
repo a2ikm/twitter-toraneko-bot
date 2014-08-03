f8b77ef04191ac475ac42162885e0c36673f991e
Masato Ikeda <masato.ikeda@gmail.com>
2014-08-03 15:50:25 +0900

    Typo

--- lita_config.rb
+++ lita_config.rb
@@ -3,7 +3,7 @@ if ENV['RACK_ENV'] !='production'
   Dotenv.load
 end
 
-MESSAGES = YAML.load_file(File.expand_path("../../messages.yml", __FILE__))
+MESSAGES = YAML.load_file(File.expand_path("../messages.yml", __FILE__))
 
 Dir[File.expand_path("../handlers/*.rb", __FILE__)].each do |file|
   require file
